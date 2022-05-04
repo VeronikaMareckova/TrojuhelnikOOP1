434a13d51749e4324b0fb347e9aed8f2b3d7e5d6
Veronika Mareckova <m-veronika@email.cz>
2022-05-04 17:42:41 +0200

    Přidejte soubory projektu.

---
+++ TrojuhelnikOOP1/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace TrojuhelnikOOP1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //OBJEKT (tim se vytvori TR ktery ma neinicializovane hodnoty promennych)
+            Trojuhelnik tr = new Trojuhelnik();
+
+            //tr.a = 5; //nejde to z toho duvodu ze ATRIBUTY PROMENNE jsou PRIVATE 
+            //tr.b = 4; //takto nemame primi pristup k polozkam TRIDY
+            //tr.c = 3; //diky tomu je zachovat princim OOP zapouzdreni
+
+
+            //Metody set umoznuji nastavit hodnoty stran trojuhelnika
+            tr.SetA(6);
+            tr.SetB(-3);
+            tr.SetC(2.5);
+
+            //Vypis
+            Console.WriteLine(tr.GetA()); //6
+            Console.WriteLine(tr.GetB()); //0
+            Console.WriteLine(tr.GetC()); //2,5
+
+
+            tr.SetAll(4, 3, 2);
+            Console.WriteLine(tr.GetA()); //4
+            Console.WriteLine(tr.GetB()); //3
+            Console.WriteLine(tr.GetC()); //2
+
+            //ulozeni do promennych (atributu)
+            bool overeni = tr.isValid();
+
+            double strana_a = tr.GetA();
+            double strana_b = tr.GetB();
+            double strana_c = tr.GetC();
+
+            double obvod = tr.spoctiObvod();
+            double obsah = tr.spoctiObsah();
+
+            Console.WriteLine("Trojúhelník {0} \n{1} \n{2} \n{3}\n{4}\n{5}", tr.isValid(), tr.GetA(), tr.GetB(), tr.GetC(), tr.spoctiObvod(), tr.spoctiObsah());
+        }
+    }
+}
---
+++ TrojuhelnikOOP1/Trojuhelnik.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TrojuhelnikOOP1
+{
+    class Trojuhelnik
+    {
+        //Atributy
+        private double a;
+        private double b;
+        private double c;
+
+        //Vlastnosti ----
+
+        //Metody GET
+        public double GetA()
+        {
+            return a;
+        }
+        public double GetB()
+        {
+            return b;
+        }
+
+        public double GetC()
+        {
+            return c;
+        }
+
+        //Metody SET (nastaveni hodnot)
+        public void SetA(double value)
+        {
+            if (value > 0.0) //podminka kladne cislo
+                a = value;
+        }
+
+        public void SetB(double value)
+        {
+            if (value > 0.0)
+                b = value;
+        }
+
+        public void SetC(double value)
+        {
+            if (value > 0.0)
+                c = value;
+        }
+
+        //metoda vola dalsi metody
+        public void SetAll(double a, double b, double c)
+        {
+            SetA(a);
+            SetB(b);
+            SetC(c);
+        }
+
+        //-----------
+        public double spoctiObvod()
+        {
+            return GetA() + GetB() + GetC(); //return a + b + c;
+        }
+
+        public double spoctiObsah()
+        {
+            double s = spoctiObvod() / 2; //obvod trojuhelniku / 2
+            return Math.Sqrt(s * (s - a) * (s - b) * (s - c)); //Heronuv vzorec
+        }
+
+        //metoda pro overeni 
+        public bool isValid()
+        {
+            //return (a + b) > c && (a + c) > b && (b + c) > a;
+            return (GetA() + GetB()) > GetC()
+                && (GetA() + GetC()) > GetB()
+                && (GetB() + GetC()) > GetA();
+
+        }
+    }
+}
